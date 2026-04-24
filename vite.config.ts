937581acb1c5f388ee94784ce9e9ac4a8b776dca
ramcakir <ramazancakir@gmail.com>
2026-04-24 03:34:03 +0200

    Set base URL to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 REPLACE with your actual GitHub repo name (e.g., '/spotter/')
-const REPO_NAME = '/spotter/'
-
 export default defineConfig({
-  base: import.meta.env.PROD ? REPO_NAME : '/',
+  base: '/spotter/', // 👈 Must match repo name
   plugins: [react()],
 })
